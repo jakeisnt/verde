32f60760603ec0474f450bf7dcb634ff311c3656
Jacob Chvatal <jake@isnt.online>
2021-03-23 10:34:01 -0700

    add opinionated eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,29 @@
 module.exports = {
-  extends: ['airbnb', 'eslint-config-prettier'],
-  plugins: ['prettier'],
+  extends: ["airbnb", "eslint-config-prettier"],
+  plugins: ["prettier"],
   rules: {
-    'prettier/prettier': [
-      'error',
+    "prettier/prettier": [
+      "error",
       {
-        singleQuote: true,
+        singleQuote: false,
         printWidth: 80,
-        arrowParens: 'avoid',
-        trailingComma: 'all',
-        endOfLine: 'auto',
+        arrowParens: "avoid",
+        trailingComma: "all",
+        endOfLine: "auto",
       },
     ],
     // incompatible with prettier
-    'react/jsx-curly-newline': 'off',
-    'react/jsx-indent': 'off',
-    'react/jsx-wrap-multilines': 'off',
-    'react/jsx-closing-tag-location': 'off',
-    'react/no-array-index-key': 'warn',
-    '@typescript-eslint/indent': 'off',
+    "react/jsx-curly-newline": "off",
+    "react/jsx-indent": "off",
+    "react/jsx-wrap-multilines": "off",
+    "react/jsx-closing-tag-location": "off",
+    "react/no-array-index-key": "warn",
+    "@typescript-eslint/indent": "off",
 
     // personal preference
-    'react/jsx-props-no-spreading': 'off',
+    "react/jsx-props-no-spreading": "off",
+
+    // react 17 doesn't need to import it : )
+    "react/react-in-jsx-scope": "off",
   },
 };
