92559aa6216193fd95b8fd591e3d373723b4e87b
Jacob Chvatal <jake@isnt.online>
2021-03-24 10:34:34 -0700

    add styles to Home

--- packages/client/src/pages/Home.jsx
+++ packages/client/src/pages/Home.jsx
@@ -1,12 +1,22 @@
 import { useHistory } from "react-router-dom";
 import { createUseStyles, useTheme } from "react-jss";
 
-const useStyles = createUseStyles({
+const useStyles = createUseStyles((theme) => ({
   home: {
     display: "flex",
-    flexDirection: "row",
+    justifyContent: "center",
+    flexDirection: "column",
+    maxWidth: "50%",
+    margin: "auto",
   },
-});
+  button: {
+    padding: "1em",
+    margin: "1em",
+    border: `2px solid ${theme.black}`,
+    color: theme.black,
+    backgroundColor: theme.white,
+  },
+}));
 
 function Home() {
   const history = useHistory();
@@ -19,10 +29,14 @@ function Home() {
 
   return (
     <div className={classes.home}>
-      <button type="button" onClick={getNewRoom}>
+      <button type="button" className={classes.button} onClick={getNewRoom}>
         Create Room
       </button>
-      <button type="button" onClick={() => history.push("/join")}>
+      <button
+        type="button"
+        className={classes.button}
+        onClick={() => history.push("/join")}
+      >
         Join Room
       </button>
     </div>
