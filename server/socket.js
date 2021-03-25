523667f8fa0cce3bc087a9247831c0c084d6c014
Jacob Chvatal <jake@isnt.online>
2021-03-24 20:22:17 -0700

    good stopping point, clients not getting msgs?

--- server/socket.js
+++ server/socket.js
@@ -2,20 +2,30 @@ const ws = require("ws");
 const url = require("url");
 const querystring = require("querystring");
 const wss = new ws.Server({ noServer: true });
+const rooms = require("./engine/rooms");
 
-// {roomName: server}
 const socketServers = {};
 
-function makeSocket() {
+function makeSocket(name) {
   const socket = new ws.Server({ noServer: true });
-  socket.on("connection", (ws) => {});
+  socket.on("connection", (ws) => {
+    console.log("received a new connection!");
+  });
   socket.on("message", (msg) => {
     console.log(`received ${msg}`);
-    socket.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    });
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new-user":
+        console.log("Received a new user", message);
+        socket.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(rooms.getRoom(name));
+          }
+        });
+        break;
+      default:
+        console.log(`"${message.type}" is not a valid socket message type.`);
+    }
   });
   return socket;
 }
@@ -25,7 +35,7 @@ function onUpgrade(request, socket, head) {
   const room = querystring.parse(url.parse(request.url).query).room;
 
   if (!socketServers[room]) {
-    socketServers[room] = makeSocket();
+    socketServers[room] = makeSocket(room);
     console.log(`Created socket for room ${room}`);
   } else {
     console.log(`Using socket for room ${room}`);
