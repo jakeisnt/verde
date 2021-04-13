b00959fd105f07892d4170ff35a557f1a06b4319
Jacob Chvatal <jake@isnt.online>
2021-04-12 21:19:11 -0700

    aahhh

--- config-overrides.js
+++ config-overrides.js
@@ -2,8 +2,7 @@ const { override, addBabelPlugins } = require("customize-cra");
 
 module.exports = override(
   addBabelPlugins(
-    // "@babel/plugin-proposal-nullish-coalescing-operator",
-    // "@babel/plugin-syntax-optional-chaining",
+    "@babel/plugin-proposal-class-properties",
     "@babel/plugin-syntax-class-properties"
   )
 );
