f4a0db540aaf4e56d0c94e9cbca9979ba0e23ea3
Jacob Chvatal <jake@isnt.online>
2021-04-16 12:27:45 -0700

    add support for passing arbitrary data to takeAction

--- server/game.js
+++ server/game.js
@@ -18,7 +18,7 @@ const game = {
   /* Specifies the different actions that players can take during the game. */
   actions: {
     // With an action, the user can:
-    "+1": (gameState, players, playerId) => ({
+    "+1": (gameState, players, playerId, payload) => ({
       // change the game state,
       gameState: gameState + 1,
       // the player state,
@@ -32,7 +32,14 @@ const game = {
       // They aren't required to provide any of these parameters,
       // as if they are not defined they just won't be used.
     }),
-    "-1": (gameState, players, playerId) => ({
+    "-1": (gameState, players, playerId, payload) => ({
+      // The payload argument contains all of the data passed with the call.
+      // Currently, `payload.data` contains all of the arguments passed to the function 'takeAction',
+        // so this should be good enough to pass any arbitrary data. I'd recommend the following:
+        // function call from client: takeAction("+1", { userId: 123 })
+        // retrieve userId here: payload.data[1].userId (0 is going to be the "+1")
+        // you could also call takeAction("+1", userId) and you would have the id at payload.data[1],
+        // but that may be a bit more difficult to keep track of.
       gameState: gameState + 1,
       // Currently, anything you get from the state object should be
       // provided when returning.
