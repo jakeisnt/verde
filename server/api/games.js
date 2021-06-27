5b6da6615f64d8f9a4f893806beb6e910265bd83
Jacob Chvatal <jake@isnt.online>
2021-06-27 16:07:44 -0700

    resave games

--- server/api/games.js
+++ server/api/games.js
@@ -1,8 +1,8 @@
 import Rooms from "../engine/rooms";
 import { logger } from "../logger";
 
-/** Presents all of the actions that can be taken during a game.
-* include a static method here that accepts a name, playerId and an optional payload (JS object)
+/** presents all of the actions that can be taken during a game.
+* include a static method here that accepts a name, playerid and an optional payload (js object)
 * to make it available for the client to call and send a message with. */
 class Game {
   static startGame(name, modId) {
