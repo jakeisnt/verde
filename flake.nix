ff7b92200dfa5a2a25ebfcb49d5b624d7677c335
Jacob Chvatal <jake@isnt.online>
2022-01-03 01:24:27 -0500

    left off at post build

--- flake.nix
+++ flake.nix
@@ -37,104 +37,59 @@
       # (just writing a script)
       server-code = (pkgs.mkYarnPackage {
         src = "${./server}";
-        packageJSON = "${docsrc}/server/package.json";
-        yarnLock = "${docsrc}/server/yarn.lock";
-      }).overrideAttrs (oldAttrs:
-        let pname = oldAttrs.pname;
-        in {
-          doDist = false;
-          buildPhase = ''
-            runHook preBuild
-            shopt -s dotglob
-
-            rm deps/${pname}/node_modules
-            mkdir deps/${pname}/node_modules
-            pushd deps/${pname}/node_modules
-            ln -s ../../../node_modules/* .
-            popd
-            yarn --offline build
-            runHook postBuild
-          '';
-          installPhase = ''
-            runHook preInstall
-
-            mv deps/${pname}/build $out
-
-            runHook postInstall
-          '';
-        });
-
+        packageJSON = "${./server/package.json}";
+        yarnLock = "${./yarn.lock}";
+        yarnPostBuild = "cd $out/deps/server/deps/server/ && node --experimental-specifier-resolution=node gen-api.js";
+      });
 
       client-code = (pkgs.mkYarnPackage {
         src = "${./client}";
         packageJSON = "${docsrc}/client/package.json";
-        yarnLock = "${docsrc}/client/yarn.lock";
-      }).overrideAttrs (oldAttrs:
-        let pname = oldAttrs.pname;
-        in {
-          doDist = false;
-          buildPhase = ''
-            runHook preBuild
-            shopt -s dotglob
-
-            rm deps/${pname}/node_modules
-            mkdir deps/${pname}/node_modules
-            pushd deps/${pname}/node_modules
-            ln -s ../../../node_modules/* .
-            popd
-            yarn --offline build
-            runHook postBuild
-          '';
-          installPhase = ''
-            runHook preInstall
-
-            mv deps/${pname}/build $out
-
-            runHook postInstall
-          '';
-        });
+        yarnLock = "${./yarn.lock}";
+        extraBuildInputs = [ server-code ];
+      });
 
       # run this service as a nixos module
-      turnBasedGameModule = { config, options, lib, pkgs, ... }:
-        with lib;
-        let cfg = config.turn-game;
-            user = "turn-game";
-            group = "turn-game";
-        in {
-          options.turn-game = with lib; {
-            enable = mkEnableOption false;
-          };
+      # turnBasedGameModule = { config, options, lib, pkgs, ... }:
+      #   with lib;
+      #   let cfg = config.turn-game;
+      #       user = "turn-game";
+      #       group = "turn-game";
+      #   in {
+      #     options.turn-game = with lib; {
+      #       enable = mkEnableOption false;
+      #     };
 
-          config = mkIf cfg.enable {
-            # create a user in which to run the web app
-            users.users.turn-game = {
-              inherit group;
-              isSystemUser = true;
-            };
+      #     config = mkIf cfg.enable {
+      #       # create a user in which to run the web app
+      #       users.users.turn-game = {
+      #         inherit group;
+      #         isSystemUser = true;
+      #       };
 
-            users.groups.turn-based-game = {};
+      #       users.groups.turn-based-game = {};
 
-            # configure a systemd service to launh it
-            systemd.services.turn-game = {
-              enable = true;
-              aliases = [ "turn-game" ];
-              after = [ "network.target" ];
-              path = with pkgs; [ openssl ];
-              serviceConfig = {
-                User = user;
-                Group = group;
-                ExecStart = "${isntweb-bundle}/bin/isntweb-serve";
-                PrivateTmp = "true";
-                PrivateDevices = "true";
-                ProtectHome = "true";
-                ProtectSystem = "strict";
-                StateDirectory = "turn-game";
-              };
-              wantedBy = [ "multi-user.target" ];
-            };
-          };
-        }
-      ;
+      #       # configure a systemd service to launh it
+      #       systemd.services.turn-game = {
+      #         enable = true;
+      #         aliases = [ "turn-game" ];
+      #         after = [ "network.target" ];
+      #         path = with pkgs; [ openssl ];
+      #         serviceConfig = {
+      #           User = user;
+      #           Group = group;
+      #           ExecStart = "${isntweb-bundle}/bin/isntweb-serve";
+      #           PrivateTmp = "true";
+      #           PrivateDevices = "true";
+      #           ProtectHome = "true";
+      #           ProtectSystem = "strict";
+      #           StateDirectory = "turn-game";
+      #         };
+      #         wantedBy = [ "multi-user.target" ];
+      #       };
+      #     };
+      #   }
+      # ;
       # 1. `yarn` in root directory to install dependencies
       # 2. yarn build
 
@@ -142,10 +97,10 @@
     in rec {
       # packages.turn-game = isntweb-bundle;
       # apps.turn-game = packages.isntweb-home;
-      # defaultPackage.${system} = isntweb-bundle;
+      defaultPackage.${system} = client-code;
 
-      nixosModules.turn-game = turnBasedGameModule;
-      nixosModule = nixosModules.turn-game;
+      # nixosModules.turn-game = turnBasedGameModule;
+      # nixosModule = nixosModules.turn-game;
 
       devShell.${system} = with pkgs; mkShell {
         buildInputs = [
