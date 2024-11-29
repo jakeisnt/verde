0d491e11d67a40b25fa8c768d633117f79f537d2
Jacob Chvatal <jake@isnt.online>
2024-11-29 21:53:38 +0100

    fix vite situation

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,1 +1,15 @@
- 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    strictPort: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:4000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
